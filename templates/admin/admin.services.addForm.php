b2b3960ea2d981254f283088ff6edf45d9d27553
Hossein Khajvandi <h.khajvandi@gmail.com>
2018-01-31 21:20:51 +0330

    add

--- templates/admin/admin.services.addForm.php
+++ templates/admin/admin.services.addForm.php
@@ -38,28 +38,27 @@
 <div class="content-control">
     <!--control-nav-->
     <ul class="control-nav pull-right">
-        <li><a class="rtl text-24"><i class="fa fa-file-image-o"></i> افزودن سرویس جدید</a></li>
+        <li><a class="rtl text-24"><i class="sidebar-icon fa fa-adn"></i> افزودن سرویس جدید</a></li>
     </ul><!--/control-nav-->
 </div><!-- /content-control -->
 
 <div class="content-body">
-
     <div id="panel-tablesorter" class="panel panel-warning">
         <div class="panel-heading bg-white">
-            <h3 class="panel-title rtl">فرم سرویس</h3>
+            <h3 class="panel-title rtl">مقاله جدید</h3>
             <div class="panel-actions">
                 <button data-expand="#panel-tablesorter" title="" class="btn-panel rtl" data-original-title="تمام صفحه">
                     <i class="fa fa-expand"></i>
                 </button>
-                <button data-collapse="#panel-tablesorter" title="" class="btn-panel rtl" data-original-title="باز و بسته شدن">
+                <button data-collapse="#panel-tablesorter" title="" class="btn-panel rtl"
+                        data-original-title="باز و بسته شدن">
                     <i class="fa fa-caret-down"></i>
                 </button>
             </div><!-- /panel-actions -->
         </div><!-- /panel-heading -->
 
-        <?php if($msg!=null)
-        {
-            ?>
+        <?php if($msg != null)
+        {?>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 alert alert-warning">
                 <?= $msg ?>
             </div>
@@ -68,34 +67,58 @@
         ?>
         <div class="panel-body">
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-8  center-block">
-                    <form name="queue" id="queue" role="form" data-validate="form"  enctype="multipart/form-data" class="form-horizontal form-bordered"  novalidate="novalidate" method="post">
+                <div class="col-xs-12 col-sm-12 col-md-8 center-block">
+                    <form name="queue" id="queue" role="form" data-validate="form" enctype="multipart/form-data"  class="form-horizontal form-bordered"
+                          novalidate="novalidate" method="post">
+
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-6">
                                 <div class="form-group">
-                                    <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="title">عنوان:</label>
-                                    <div class="col-xs-12 col-sm-8 pull-right">
-                                        <input type="text" class="form-control" name="title" id="title"   value="<?=$list['title']?>">
+                                    <label class="col-xs-12 col-sm-4 col-md-4 pull-right control-label rtl"
+                                           for="title">نام :</label>
+                                    <div class="col-xs-12 col-sm-8 col-md-8 pull-right">
+                                        <input type="text" class="form-control" name="title" id="title"  value="<?= $list['title'] ?>">
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-md-6">
-                                <div class="form-group">
-                                    <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="brief_description">توضیحات مختصر:</label>
-                                    <div class="col-xs-12 col-sm-8 pull-right">
-                                        <input type="text" class="form-control" name="brief_description"  id="brief_description"  value="<?=$list['brif_description']?>">
-                                    </div>
-                                </div>
-                            </div>
+
                         </div>
+
                         <div class="row xsmallSpace hidden-xs"></div>
                         <div class="row">
-
                             <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <label class="col-xs-12 col-sm-1 col-md-1 pull-right control-label rtl"
-                                           for="description">توضیحات</label>
-                                    <div class="col-xs-12 col-sm-11 col-md-11 pull-right">
+
+                                    <div class="col-xs-12 col-sm-12 col-md-12 pull-right">
+                                        <h3> توضیح مختصر</h3>
+                                        <?php
+
+                                        include_once ROOT_DIR.'common/ckeditor/ckeditor.php';
+                                        include_once ROOT_DIR.'common/ckfinder/ckfinder.php';
+                                        $ckeditor = new CKEditor();
+                                        $ckeditor->basePath = RELA_DIR.'common/ckeditor/';
+
+
+
+
+                                        $config['language'] = 'fa';
+                                        $config['filebrowserBrowseUrl'] = RELA_DIR.'common/ckfinder/ckfinder.html';
+                                        $config['filebrowserImageBrowseUrl'] = RELA_DIR.'common/ckfinder/ckfinder.html?type=Images';
+                                        $config['filebrowserUploadUrl'] = RELA_DIR.'common/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+                                        $config['filebrowserImageUploadUrl'] = RELA_DIR.'common/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+
+                                        $tt = $ckeditor->editor('brief_description',$list['brief_description'],$config);
+
+                                        echo $tt;
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+
+                                    <div class="col-xs-12 col-sm-12 col-md-12 pull-right">
+                                        <h3>توضیح</h3>
 
                                         <?php
 
@@ -123,33 +146,36 @@
                             <div class="row">
                                 <div class="col-xs-12 col-sm-12 col-md-6">
                                     <div class="form-group">
-                                        <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="meta_keywords">Meta keyword:</label>
-                                        <div class="col-xs-12 col-sm-8 pull-right">
-                                            <input type="text" class="form-control" name="meta_keywords" id="meta_keywords"   value="<?=$list['meta_keywords']?>">
+                                        <label class="col-xs-12 col-sm-4 col-md-4 pull-right control-label rtl"
+                                               for="meta_keywords">Meta keyword :</label>
+                                        <div class="col-xs-12 col-sm-8 col-md-8 pull-right">
+                                            <input type="text" class="form-control" name="meta_keywords" id="meta_keywords"  value="<?= $list['meta_keywords'] ?>">
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-xs-12 col-sm-12 col-md-6">
                                     <div class="form-group">
-                                        <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="Meta_description">meta description:</label>
-                                        <div class="col-xs-12 col-sm-8 pull-right">
-                                            <input type="text" class="form-control" name="Meta_description"  id="Meta_description"  value="<?=$list['Meta_description']?>">
+                                        <label class="col-xs-12 col-sm-4 col-md-4 pull-right control-label rtl"
+                                               for="meta_description">Meta description:</label>
+                                        <div class="col-xs-12 col-sm-8 col-md-8 pull-right">
+                                            <input type="text" class="form-control" name="meta_description" id="meta_description" value="<?= $list['meta_description'] ?>">
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="row xsmallSpace hidden-xs"></div>
                             <div class="col-xs-12 col-sm-12 col-md-6">
                                 <div class="form-group">
                                     <label class="col-xs-12 col-sm-4 pull-right control-label rtl"
                                            for="category_id">انتخاب دسته بندی:</label>
                                     <div class="col-xs-12 col-sm-8 pull-right">
+
                                         <select name="category_id[]" id="category_id" data-input="select2" multiple>
                                             <?
+
                                             foreach($list['category'] as $category_id => $value)
                                             {
                                                 ?>
-                                                <option <?php echo in_array($value['Category_id'], $list['category_id']) ? 'selected' : '' ?>
+                                                <option <?php echo in_array($value['Category_id'], explode(',',$list['category_id'])) ? 'selected' : '' ?>
                                                         value="<?= $value['Category_id'] ?>">
                                                     <?= $value['export'] ?>
                                                 </option>
@@ -161,36 +187,35 @@
                                 </div>
                             </div>
                         </div>
-
-
                         <div class="row xsmallSpace hidden-xs"></div>
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-6">
                                 <div class="form-group">
-                                    <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="xImagePath">تصویر:</label>
+                                    <label class="col-xs-12 col-sm-4 pull-right control-label rtl"
+                                           for="xImagePath">تصویر:</label>
                                     <div class="col-xs-12 col-sm-8 pull-right">
-                                        <input type="file" name="image">
+                                        <div class="input-group" dir="ltr">
+                                            <input type="file" class="form-control" name="image" >
+
+                                            <img src="<?=RELA_DIR?>statics/services/<?=$list['image']?>" class="img-responsive">
+                                        </div>
+
+
+
                                     </div>
                                 </div>
                             </div>
+
                         </div>
-                        <div class="row xsmallSpace hidden-xs"></div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-6">
-                                <div class="form-group">
-                                    <label class="col-xs-12 col-sm-4 pull-right control-label rtl" for="xImagePath">تصویر سایز کوچک:</label>
-                                    <div class="col-xs-12 col-sm-8 pull-right">
-                                        <input type="file" name="imagesmall">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
+
 
                         <div class="row">
                             <div class="col-md-12">
                                 <p class="pull-right">
-                                    <button type="submit"  class="btn btn-icon btn-success rtl">
-                                        <input name="action" type="hidden" id="action" value="add" />
+                                    <button type="submit" name="update" id="submit"
+                                            class="btn btn-icon btn-success rtl"><input name="action" type="hidden" id="action" value="add"/>
+                                        <input type="hidden" name="Services_id" value="<?=$list['Services_id']?>">
                                         <i class="fa fa-plus"></i>
                                         ثبت
                                     </button>
